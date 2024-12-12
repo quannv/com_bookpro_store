1f90dcb5830e59d85dacbbc3014c9651bceaf8c7
JOOMBOOKING <quannv@gmail.com>
2024-12-12 20:56:21 +0700

    ok

---
+++ src/Table/DesignTable.php
@@ -0,0 +1,121 @@
+<?php
+
+/**
+ * @package 	Bookpro
+ * @author 		Ngo Van Quan
+ * @link 		http://joombooking.com
+ * @copyright 	Copyright (C) 2011 - 2012 Ngo Van Quan
+ * @license 	GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ * @version 	$Id: airport.php  23-06-2012 23:33:14
+ **/
+
+namespace Joombooking\Component\Bookpro\Administrator\Table;
+
+use Exception;
+use Joomla\CMS\Application\ApplicationHelper;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Table\Table;
+use Joomla\Database\DatabaseDriver;
+use Joomla\Utilities\ArrayHelper;
+
+defined('_JEXEC') or die('Restricted access');
+
+class DesignTable extends Table
+{
+
+	public function __construct(DatabaseDriver $db)
+	{
+
+		//$this->setColumnAlias('published', 'state');
+		parent::__construct('#__bookpro_designs', 'id', $db);
+		$this->setColumnAlias('published', 'state');
+	}
+
+
+	function check()
+	{
+
+		if (empty($this->alias)) {
+			$this->alias = $this->title;
+		}
+		return true;
+	}
+
+
+
+	public function store($updateNulls = false)
+	{
+
+		return parent::store($updateNulls);
+	}
+	public function publish($pks = null, $state = 1, $userId = 0)
+	{
+		$k = $this->_tbl_key;
+
+		// Sanitize input.
+		ArrayHelper::toInteger($pks);
+		$userId = (int) $userId;
+		$state  = (int) $state;
+
+		// If there are no primary keys set check to see if the instance key is set.
+		if (empty($pks)) {
+			if ($this->$k) {
+				$pks = array($this->$k);
+			}
+			// Nothing to set publishing state on, return false.
+			else {
+				$this->setError(Text::_('JLIB_DATABASE_ERROR_NO_ROWS_SELECTED'));
+				return false;
+			}
+		}
+
+		// Build the WHERE clause for the primary keys.
+		$where = $k . '=' . implode(' OR ' . $k . '=', $pks);
+
+		// Determine if there is checkin support for the table.
+		if (!property_exists($this, 'checked_out') || !property_exists($this, 'checked_out_time')) {
+			$checkin = '';
+		} else {
+			$checkin = ' AND (checked_out = 0 OR checked_out = ' . (int) $userId . ')';
+		}
+
+		// Update the publishing state for rows with the given primary keys.
+		$this->_db->setQuery(
+			'UPDATE ' . $this->_db->quoteName($this->_tbl) .
+				' SET ' . $this->_db->quoteName('state') . ' = ' . (int) $state .
+				' WHERE (' . $where . ')' .
+				$checkin
+		);
+
+		try {
+			$this->_db->execute();
+		} catch (Exception $e) {
+			$this->setError($e->getMessage());
+			return false;
+		}
+
+		// If checkin is supported and all rows were adjusted, check them in.
+		if ($checkin && (count($pks) == $this->_db->getAffectedRows())) {
+			// Checkin the rows.
+			foreach ($pks as $pk) {
+				$this->checkin($pk);
+			}
+		}
+
+		// If the JTable instance value is in the list of primary keys that were set, set the instance.
+		if (in_array($this->$k, $pks)) {
+			$this->state = $state;
+		}
+
+		$this->setError('');
+		return true;
+	}
+
+	public function bind($array, $ignore = '')
+	{
+
+
+		return parent::bind($array, $ignore);
+	}
+}
