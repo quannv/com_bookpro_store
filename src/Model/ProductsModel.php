0770300793315d366a106e133c8607b52e0dbf10
JOOMBOOKING <quannv@gmail.com>
2024-12-13 11:01:11 +0700

    ok

--- src/Model/ProductsModel.php
+++ src/Model/ProductsModel.php
@@ -62,52 +62,28 @@ class ProductsModel extends ListModel
      */
     protected function getListQuery()
     {
-        $query = null;
 
         $db = $this->getDatabase();
-
-
         $query = $db->getQuery(true);
-
-        // get table name
-        $airportTable = $db->quoteName('#__bookpro_dest');
-
         $query->select(array(
-            'product.*,operator.shortname,operator.company',
-            'vehicle.title AS vehicle_name',
-            'dest1.code AS from_code',
-            'dest2.code AS to_code',
-            'dest1.title AS fromName',
-            'dest2.title AS toName',
-            'operator.image AS operator_logo',
+            'product.*,brands.title AS brand_title',
+            'designs.title AS design_title',
+            
 
         ));
 
-        $query->from('#__bookpro_product AS product');
-        $query->join('left', $airportTable . ' AS dest1' . ' ON product.from = dest1.id ');
-        $query->join('left', $airportTable . ' AS dest2' . ' ON product.to = dest2.id ');
-        $query->join('left', '#__bookpro_operator AS operator ON operator.id = product.operator_id ');
-        $query->join('left', '#__bookpro_vehicle AS vehicle ON vehicle.id = product.vehicle_id ');
+        $query->from('#__bookpro_products AS product');
+      
+        $query->join('left', '#__bookpro_brands AS brands ON brands.id = product.brand_id ');
+        $query->join('left', '#__bookpro_designs AS designs ON designs.id = product.design_id ');
+
+        
+        $design_id = $this->getState('filter.design_id');
+        if (!empty($vehicle_id)) {
+            $query->where('(product.design_id = ' . (int) $design_id . ')');
+        }
 
        
-
-        $from = $this->getState('filter.from');
-        if (!empty($from)) {
-            $query->where('(product.from = ' . (int) $from . ')');
-        }
-        $to = $this->getState('filter.to');
-        if (!empty($to)) {
-            $query->where('(product.to = ' . (int) $to . ')');
-        }
-        $vehicle_id = $this->getState('filter.vehicle_id');
-        if (!empty($vehicle_id)) {
-            $query->where('(product.vehicle_id = ' . (int) $vehicle_id . ')');
-        }
-
-        $operator_id = $this->getState('filter.operator_id');
-        if (!empty($operator_id)) {
-            $query->where('(product.operator_id = ' . (int) $operator_id . ')');
-        }
 
         $state = $this->getState('filter.state');
         if (!empty($state)) {
@@ -117,12 +93,15 @@ class ProductsModel extends ListModel
         $orderCol = $this->state->get('list.ordering');
         $orderDirn = $this->state->get('list.direction');
         if (empty($orderCol) || empty($orderDirn)) {
-            $orderCol = 'product.start';
+            $orderCol = 'product.id';
             $orderDirn = 'ASC';
         }
 
 
         $query->group('product.id');
+
+       // echo $db->replacePrefix((string) $query);
+        //die;
 
         return $query;
     }
