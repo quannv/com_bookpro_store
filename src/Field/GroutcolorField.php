b903e1d681080e1575638508bf38f2e579b952c3
JOOMBOOKING <quannv@gmail.com>
2024-12-16 15:48:02 +0700

    ok

---
+++ src/Field/GroutcolorField.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Popup element to select groutcolor.
+ * @package     Bookpro
+ * @subpackage  Field
+ * @author      Ngo Van Quan
+ * @link        http://joombooking.com
+ * @copyright   Copyright (C) 2011 - 2012 Ngo Van Quan
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ **/
+
+namespace Joombooking\Component\Bookpro\Administrator\Field;
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\Form\Field\ListField;
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Text;
+
+defined('_JEXEC') or die('Restricted access');
+
+class GroutcolorField extends ListField
+{
+
+    protected $type = 'Groutcolor';
+
+    public function getOptions()
+    {
+        $options = [];
+        $db = Factory::getDBO();
+        $query = $db->getQuery(true);
+
+        $query->select(
+            [
+                $db->quoteName('id', 'value'),
+                $db->quoteName('title', 'text'),
+            ]
+        )
+        ->from('#__bookpro_groutcolors')->order('title ASC');
+
+        $db->setQuery($query);
+        try {
+            $options = $db->loadObjectList();
+            array_unshift($options, HTMLHelper::_('select.option', '0', Text::_('COM_BOOKPRO_SELECT_GROUTCOLOR')));
+            return $options;
+        } catch (\RuntimeException $e) {
+            Factory::getApplication()->enqueueMessage($e->getMessage(), 'error');
+        }
+    }
+}
