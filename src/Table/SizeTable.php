c63fe4d2ecfcf9a5ba56cda2b13c83735f36a45d
JOOMBOOKING <quannv@gmail.com>
2024-12-16 14:51:24 +0700

    ok

---
+++ src/Table/SizeTable.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * @package 	Bookpro
+ * @author 		Ngo Van Quan
+ * @link 		http://joombooking.com
+ * @copyright 	Copyright (C) 2011 - 2012 Ngo Van Quan
+ * @license 	GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ * @version 	$Id: airport.php  23-06-2012 23:33:14
+ **/
+
+namespace Joombooking\Component\Bookpro\Administrator\Table;
+
+use Exception;
+use Joomla\CMS\Application\ApplicationHelper;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Table\Table;
+use Joomla\Database\DatabaseDriver;
+use Joomla\Utilities\ArrayHelper;
+
+defined('_JEXEC') or die('Restricted access');
+
+class SizeTable extends Table
+{
+
+	public function __construct(DatabaseDriver $db)
+	{
+
+		//$this->setColumnAlias('published', 'state');
+		parent::__construct('#__bookpro_sizes', 'id', $db);
+		$this->setColumnAlias('published', 'state');
+	}
+
+	
+
+	public function bind($array, $ignore = '')
+	{
+
+
+		return parent::bind($array, $ignore);
+	}
+}
